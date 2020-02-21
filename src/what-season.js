7ab39301380938dd1ee88835372bc8917c1b647e
Vasli Saidmuradov <vasli.said@gmail.com>
2020-02-21 15:42:53 +0600

    solve tasks: what season and hanoi tower

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,19 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  // const re = /new Date\(\d+, \d+, \d+, \d+, \d+, \d+, \d+\)/g
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  if (date instanceof Date && isNaN(date.valueOf())) {
+    throw new Error('Упс...')
+  }
+  const month = date.getMonth()
+  const winter = [0, 1, 11],
+    spring = [2, 3, 4],
+    summer = [5, 6, 7],
+    autumn = [8, 9, 10];
+
+  if (winter.includes(month)) return 'winter'
+  if (spring.includes(month)) return 'spring'
+  if (summer.includes(month)) return 'summer'
+  if (autumn.includes(month)) return 'autumn'
+}
